e11e9a72ae05fbaed530d4a31fb5e3018c665a5e
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-08-28 11:23:04 +0200

    feat: add first models

---
+++ BlazorAppDemo/Models/Team.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAppDemo.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public int Points { get; set; }
+        // public Country Country { get; set; }
+    }
+}
