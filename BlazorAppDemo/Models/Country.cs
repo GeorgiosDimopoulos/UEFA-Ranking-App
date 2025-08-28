cdfc612f87c80e2f5d7903ff1273e9d5f42cb09c
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-08-28 14:34:00 +0200

    feat: add Country model and set it in Teams UI

---
+++ BlazorAppDemo/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppDemo.Models
+{
+    public class Country
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Id { get; set; }
+        public IEnumerable<Team> Teams { get; set; } = [];
+    }
+}
