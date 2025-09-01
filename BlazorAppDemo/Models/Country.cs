bf48d13601f675606e02aaff980e753a796ef60a
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-09-01 10:29:01 +0200

    feat: expand Countries UI columns

--- BlazorAppDemo/Models/Country.cs
+++ BlazorAppDemo/Models/Country.cs
@@ -4,6 +4,7 @@
     {
         public string Name { get; set; } = string.Empty;
         public int Id { get; set; }
+        public int Position { get; set; }
         public IEnumerable<Team> Teams { get; set; } = [];
         public int NumberOfTeams => Teams.Count();
         public int TotalPoints => Teams.Sum(t => t.Points);
