60725184cfeaf00d054485c8933b337d95bf3593
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-09-01 17:00:02 +0200

    feat: add inner country's Team component

--- Core/DataAccess/Databasefeeder.cs
+++ Core/DataAccess/Databasefeeder.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Core.Data.Models;
+﻿using Core.Data.Models;
 
 namespace Core.DataAccess
 {
@@ -13,53 +12,41 @@ namespace Core.DataAccess
             try
             {
                 Countries = [
-                    new()
-                    {
-                        Id = 1,
-                        Name = "Greece",
-                        Position = 11
-                    },
-                    new()
-                    {
-                        Id = 2,
-                        Name = "Germany",
-                        Position = 3
-                    },
-                    new()
-                    {
-                        Id = 3,
-                        Name = "Spain",
-                        Position = 2
-                    }];
+                    new() { Id = 2, Name = "England", Position = 1 },
+                    new() { Id = 3, Name = "Spain", Position = 2 },
+                    new() { Id = 2, Name = "Germany", Position = 3 },
+                    new() { Id = 2, Name = "Italy", Position = 4 },
+                    new() { Id = 2, Name = "France", Position = 5 },
+                    new() { Id = 2, Name = "Portugal", Position = 6 },
+                    new() { Id = 2, Name = "Netherlands", Position = 7 },
+                    new() { Id = 2, Name = "Belgium", Position = 8 },
+                    new() { Id = 2, Name = "Turkey", Position = 9 },
+                    new() { Id = 2, Name = "Czech", Position = 10 },
+                    new() { Id = 1, Name = "Greece", Position = 11 }];
 
                 Teams = [
-                    new Team
-                    {
+                    new Team {
                         Name = "AEK",
                         Competition = Competition.ConferenceLeague,
-                        Country = Countries.First(c => c.Name.Equals("Greece")),
+                        Country = Countries.FirstOrDefault(c => c.Name.Equals("Greece")) ?? throw new InvalidOperationException("Country not found"),
                         IsActive = true,
                         Id = 1,
-                        Points = 2
-                        },
-                    new Team
-                    {
+                        Points = 2 },
+                    new Team {
                         Name = "Vfb",
                         Competition = Competition.EuropaLeague,
-                        Country = Countries.First(c => c.Name.Equals("Germany")),
+                        Country = Countries.FirstOrDefault(c => c.Name.Equals("Germany")) ?? throw new InvalidOperationException("Country not found"),
                         IsActive = false,
                         Id = 2,
-                        Points = 0
-                    },
-                    new Team
-                    {
+                        Points = 0 },
+                    new Team {
                         Name = "Sevilla",
                         Competition = Competition.None,
-                        Country = Countries.First(c => c.Name.Equals("Spain")),
+                        Country = Countries.FirstOrDefault(c => c.Name.Equals("Spain")) ?? throw new InvalidOperationException("Country not found"),
                         IsActive = false,
                         Id = 2,
                         Points = 0
-                }];
+                    }];
 
                 foreach (var c in Countries)
                 {
