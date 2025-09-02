dd613f700bfe4e971d0856cea3e84dea4ab2d7fa
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-09-02 15:24:56 +0200

    feat: add Swagger Doc in API layer

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
                                   
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection();                                                                                               
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => "Welcome to UEFA Ranking API");
 
 app.Run();
