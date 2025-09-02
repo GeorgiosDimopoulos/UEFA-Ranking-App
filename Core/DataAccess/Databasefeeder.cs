6d81b29889d189e4be5925256b8ee7296d8b0c2f
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-09-02 09:37:41 +0200

    feat: add expanded view for Country teams

--- Core/DataAccess/Databasefeeder.cs
+++ Core/DataAccess/Databasefeeder.cs
@@ -12,17 +12,17 @@ namespace Core.DataAccess
             try
             {
                 Countries = [
-                    new() { Id = 2, Name = "England", Position = 1 },
-                    new() { Id = 3, Name = "Spain", Position = 2 },
-                    new() { Id = 2, Name = "Germany", Position = 3 },
-                    new() { Id = 2, Name = "Italy", Position = 4 },
-                    new() { Id = 2, Name = "France", Position = 5 },
-                    new() { Id = 2, Name = "Portugal", Position = 6 },
-                    new() { Id = 2, Name = "Netherlands", Position = 7 },
-                    new() { Id = 2, Name = "Belgium", Position = 8 },
-                    new() { Id = 2, Name = "Turkey", Position = 9 },
-                    new() { Id = 2, Name = "Czech", Position = 10 },
-                    new() { Id = 1, Name = "Greece", Position = 11 }];
+                    new() { Id = 1, Name = "England", Position = 1 },
+                    new() { Id = 2, Name = "Spain", Position = 2 },
+                    new() { Id = 3, Name = "Germany", Position = 3 },
+                    new() { Id = 4, Name = "Italy", Position = 4 },
+                    new() { Id = 5, Name = "France", Position = 5 },
+                    new() { Id = 6, Name = "Portugal", Position = 6 },
+                    new() { Id = 7, Name = "Netherlands", Position = 7 },
+                    new() { Id = 8, Name = "Belgium", Position = 8 },
+                    new() { Id = 9, Name = "Turkey", Position = 9 },
+                    new() { Id = 10, Name = "Czech", Position = 10 },
+                    new() { Id = 11, Name = "Greece", Position = 11 }];
 
                 Teams = [
                     new Team {
@@ -44,7 +44,7 @@ namespace Core.DataAccess
                         Competition = Competition.None,
                         Country = Countries.FirstOrDefault(c => c.Name.Equals("Spain")) ?? throw new InvalidOperationException("Country not found"),
                         IsActive = false,
-                        Id = 2,
+                        Id = 3,
                         Points = 0
                     }];
 
