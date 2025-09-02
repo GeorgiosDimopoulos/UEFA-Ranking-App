120a8921488d030fe0853e20638111b5f5640ff4
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-09-02 17:20:49 +0200

    feat: add simple DTOs models in API layer

---
+++ API/Data/DTOs/CountryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Data.DTOs;
+
+public class CountryDto
+{
+    public string Name { get; set; } = string.Empty;
+    public int Position { get; set; }
+    public int NumberOfTeams { get; set; }
+    public double TotalPoints { get; set; }
+}
---
+++ API/Data/DTOs/TeamDto.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Data.DTOs;
+
+public class TeamDto
+{
+    public string Name { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public int Points { get; set; }
+    public int Position { get; set; }
+    public int CountryId { get; set; }
+    public int Competition { get; set; }
+}
