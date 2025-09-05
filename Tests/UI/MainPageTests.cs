df38287a8a0066e7a6557d44297330e12aa56c2c
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-09-05 13:12:36 +0200

    feat: add first simple UI test for Home/MainPage

---
+++ Tests/UI/MainPageTests.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Tests.UI;
+
+public class MainPageTests
+{
+    private const string baseUrl = "https://localhost:5001/";
+
+    [Fact]
+    public void MainPage_Loaded_WithSucces()
+    {
+        var driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(baseUrl);
+        Assert.Equal("Home", driver.Title);
+
+        var heading = driver.FindElement(By.TagName("h2"));
+        Assert.Equal("Hello, football fans!", heading.Text);
+
+        var header3 = driver.FindElement(By.TagName("h3"));
+        Assert.Equal("Welcome to your simple UEFA ranking app for 2025", header3.Text);
+    }
+}
