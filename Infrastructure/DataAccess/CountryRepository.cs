3108a4aee6be0e40e5f26c79ce180e793d3315f5
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2025-09-07 15:05:59 +0200

    feat: set Save() in CountryDetails

--- Infrastructure/DataAccess/CountryRepository.cs
+++ Infrastructure/DataAccess/CountryRepository.cs
@@ -64,9 +64,9 @@ public class CountryRepository : ICountryRepository
     {
         using var connection = new SqliteConnection(_connectionString);
 
-        var updateCountryQuery = "UPDATE Countries SET Name = @Name, Position = @Position WHERE Id = @Id";
+        var updateCountryQuery = "UPDATE Countries SET Name = @Name, Position = @Position, NumberOfInitialTeams= @NumberOfInitialTeams WHERE Id = @Id";
 
-        var result = await connection.ExecuteAsync(updateCountryQuery, new { c.Name, c.Position, Id = id, c.TotalPoints, c.NumberOfTeams});
+        var result = await connection.ExecuteAsync(updateCountryQuery, new { c.Name, c.Position, Id = id, c.TotalPoints, c.NumberOfInitialTeams});
         return result > 0;
     }
 
